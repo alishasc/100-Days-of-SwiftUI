02dbea7e0c449ba31e0cacbcf6364938ff2ea0c4
Alisha Carrington <alishasc2001@icloud.com>
2025-09-09 13:02:42 +0100

    Set up project 18 - Layout and Geometry

---
+++ 92-94 - Project 18 LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Alisha Carrington on 09/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
