ea3d64e35fb3467e99081372f508e2ead8d464fb
Alisha Carrington <alishasc2001@icloud.com>
2025-09-16 10:15:50 +0100

    Proect 19 setup

---
+++ 96-99 - Project 19 SnowSeeker/SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Alisha Carrington on 16/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
